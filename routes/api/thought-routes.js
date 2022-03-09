983342a7e7cb6c0f9545e2b715ccf877b918407a
noahfred97 <90789790+noahfred97@users.noreply.github.com>
2022-03-09 17:32:16 -0500

    fixed2

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -12,17 +12,17 @@ const {
 
 router.route("/").get(getAllThoughts);
 
-router.route("/:userId").post(addThought);
+router.route("/:./userId").post(addThought);
 
 router
-  .route("/:thoughtId")
+  .route("/:./thoughtId")
   .get(getThoughtById)
   .put(updateThought)
   .delete(removeThought);
 
 
-router.route("/:thoughtId/reactions").post(addReaction);
+router.route("/:./thoughtId/reactions").post(addReaction);
 
-router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+router.route("/:./thoughtId/reactions/:reactionId").delete(removeReaction);
 
 module.exports = router;
